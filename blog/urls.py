a1845a465d5b656ba5f633641c000e1572136fd4
Nisha Pant <nisha007pant@gmail.com>
2017-07-20 13:47:51 -0500

    update

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,19 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
