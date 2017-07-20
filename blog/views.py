c1ca3f404c655382c0398de330edb75577f59044
Nisha Pant <nisha007pant@gmail.com>
2017-07-20 13:57:19 -0500

    update

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
+
 
 
 # Create your views here.
